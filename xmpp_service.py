07131dfa5a32c2697214d990daea5fa40d795c41
Joel Lee <bcse@bcse.info>
2010-11-27 01:33:12 +0800

    Just having more love with TAB

--- xmpp_service.py
+++ xmpp_service.py
@@ -5,20 +5,20 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 import simplejson as json
 
 class XMPPHandler(webapp.RequestHandler):
-    def post(self):
-        message = xmpp.Message(self.request.POST)
-        try:
-            result = json.loads(message.body)
-        except:
-            message.reply(json.dumps({'error': 'JSON parse error.'}))
-        else:
-            message.reply(json.dumps(result))
+	def post(self):
+		message = xmpp.Message(self.request.POST)
+		try:
+			result = json.loads(message.body)
+		except:
+			message.reply(json.dumps({'error': 'JSON parse error.'}))
+		else:
+			message.reply(json.dumps(result))
 
 application = webapp.WSGIApplication([('/_ah/xmpp/message/chat/', XMPPHandler)],
                                      debug=True)
 
 def main():
-    run_wsgi_app(application)
+	run_wsgi_app(application)
 
 if __name__ == '__main__':
-    main()
+	main()
