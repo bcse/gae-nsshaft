3003d248415a2e12f782b12a31a88e1a0224aa24
Joel Lee <bcse@bcse.info>
2010-11-28 04:27:38 +0800

    Add some placeholders.

--- controller.py
+++ controller.py
@@ -5,14 +5,70 @@ class Controller:
 	def list_room(arg):
 		return {
 			'response': 'list_room',
-			'msg': arg}
+			'msg': 'Not Implemented.'}
+
+
+	@staticmethod
+	def create_room(arg):
+		return {
+			'response': 'list_room',
+			'msg': 'Not Implemented.'}
+
+
+	@staticmethod
+	def list_player(arg):
+		return {
+			'response': 'list_game',
+			'msg': 'Not Implemented.'}
+
+
+	@staticmethod
+	def join_room(arg):
+		return {
+			'response': 'list_game',
+			'msg': 'Not Implemented.'}
+
+
+	@staticmethod
+	def leave_room(arg):
+		return {
+			'response': 'list_game',
+			'msg': 'Not Implemented.'}
+
+
+	@staticmethod
+	def ready_game(arg):
+		return {
+			'response': 'list_game',
+			'msg': 'Not Implemented.'}
+
+
+	@staticmethod
+	def update_status(arg):
+		return {
+			'response': 'list_game',
+			'msg': 'Not Implemented.'}
 
 
 	@staticmethod
 	def list_game(arg):
 		return {
 			'response': 'list_game',
-			'msg': arg}
+			'msg': 'Not Implemented.'}
+
+
+	@staticmethod
+	def start_game(arg):
+		return {
+			'response': 'list_game',
+			'msg': 'Not Implemented.'}
+
+
+	@staticmethod
+	def end_game(arg):
+		return {
+			'response': 'list_game',
+			'msg': 'Not Implemented.'}
 
 
 	@staticmethod
