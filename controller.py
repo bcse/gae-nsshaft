df2d4bf0f0f1351d46fd214aea4186a0b1fa7e7b
Joel Lee <bcse@bcse.info>
2010-11-28 04:37:04 +0800

    Fix previous commit

--- controller.py
+++ controller.py
@@ -11,42 +11,42 @@ class Controller:
 	@staticmethod
 	def create_room(arg):
 		return {
-			'response': 'list_room',
+			'response': 'create_room',
 			'msg': 'Not Implemented.'}
 
 
 	@staticmethod
 	def list_player(arg):
 		return {
-			'response': 'list_game',
+			'response': 'list_player',
 			'msg': 'Not Implemented.'}
 
 
 	@staticmethod
 	def join_room(arg):
 		return {
-			'response': 'list_game',
+			'response': 'join_room',
 			'msg': 'Not Implemented.'}
 
 
 	@staticmethod
 	def leave_room(arg):
 		return {
-			'response': 'list_game',
+			'response': 'leave_room',
 			'msg': 'Not Implemented.'}
 
 
 	@staticmethod
 	def ready_game(arg):
 		return {
-			'response': 'list_game',
+			'response': 'ready_game',
 			'msg': 'Not Implemented.'}
 
 
 	@staticmethod
 	def update_status(arg):
 		return {
-			'response': 'list_game',
+			'response': 'update_status',
 			'msg': 'Not Implemented.'}
 
 
@@ -60,14 +60,14 @@ class Controller:
 	@staticmethod
 	def start_game(arg):
 		return {
-			'response': 'list_game',
+			'response': 'start_game',
 			'msg': 'Not Implemented.'}
 
 
 	@staticmethod
 	def end_game(arg):
 		return {
-			'response': 'list_game',
+			'response': 'end_game',
 			'msg': 'Not Implemented.'}
 
 
